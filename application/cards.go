f2eaab8db6dddba73bd990bac380045691921160
Danny Hermes <daniel.j.hermes@gmail.com>
2015-02-02 19:37:27 -0800

    Adding basic card type.

---
+++ application/cards.go
@@ -0,0 +1,84 @@
+package hukabuk
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	suits = map[byte]bool{
+		'H': true,
+		'S': true,
+		'C': true,
+		'D': true,
+	}
+	ranks = map[byte]uint8{ // Use 0 as empty key
+		'2': 1,
+		'3': 2,
+		'4': 3,
+		'5': 4,
+		'6': 5,
+		'7': 6,
+		'8': 7,
+		'9': 8,
+		'T': 9,
+		'J': 10,
+		'Q': 11,
+		'K': 12,
+		'A': 13,
+	}
+)
+
+type Card struct {
+	Suit byte
+	Rank byte
+}
+
+// Make a new Card. Verifies that the suit and rank are among
+// the acceptable values (single bytes) where 'T', 'J', 'Q', 'K', 'A'
+// are used for ranks outside of 2-9 and 'H' -> Heart, 'S' -> Spades,
+// 'C' -> Clubs and 'D' -> Diamonds for suits.
+func NewCard(suit byte, rank byte) (*Card, error) {
+	card := &Card{}
+
+	if !suits[suit] {
+		return nil, errors.New(fmt.Sprintf("Invalid suit: %q.", suit))
+	}
+	card.Suit = suit
+
+	if ranks[rank] == 0 {
+		return nil, errors.New(fmt.Sprintf("Invalid rank: %q.", rank))
+	}
+	card.Rank = rank
+	return card, nil
+}
+
+// IsBetter compares a card to another during a hand of Huk-A-Buk. In
+// addition to the cards themselves, the trump for the hand and the
+// suit led on the current trick must be incorporated. In the case that
+// both cards don't match the trump or the lead suit, no comparison
+// between the two is relevant.
+func (card *Card) IsBetter(other *Card, trump byte, lead byte) bool {
+	if card.Suit == other.Suit {
+		return ranks[card.Rank] > ranks[other.Rank]
+	}
+
+	// If the suits are different, then at most 1 is trump and at
+	// most 1 is the lead suit.
+	// Trump has higher precedence than lead (which may also be trump).
+	if card.Suit == trump {
+		return true
+	} else if other.Suit == trump {
+		return false
+	}
+
+	if card.Suit == lead {
+		return true
+	} else if other.Suit == lead {
+		return false
+	}
+
+	// If neither card is one of the relevant suits, their comparison
+	// is irrelevant.
+	return false
+}
--- application/main.go
+++ application/main.go
@@ -1,4 +1,4 @@
-package hello
+package hukabuk
 
 import (
 	"fmt"
@@ -10,5 +10,12 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Huk-A-Buk Will Go Here!")
+	c1, _ := NewCard('H', '2')
+	c2, _ := NewCard('H', '5')
+	if c1.IsBetter(c2, 'S', 'D') {
+		fmt.Fprint(w, "Card 1 is better.\n")
+	} else {
+		fmt.Fprint(w, "Card 1 is not better.\n")
+	}
+	fmt.Fprint(w, "Yay for Huk-A-Buk!")
 }
