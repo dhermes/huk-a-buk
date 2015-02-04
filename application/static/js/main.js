94c4eb41b00acc0dc40410ac0dbb89c752c3f0d5
Danny Hermes <daniel.j.hermes@gmail.com>
2015-02-03 23:03:17 -0800

    Adding base frame for games.

--- application/static/js/main.js
+++ application/static/js/main.js
@@ -1,6 +1,17 @@
 var hukABukApp = hukABukApp || {};
 
+hukABukApp.getGames = function() {
+};
+
 hukABukApp.mainSigninCallback = function(authResult) {
+  if (hukABukApp.baseSigninCallback(authResult)) {
+    document.getElementById('games').classList.remove('hidden');
+    var apiRoot = '//' + window.location.host + '/_ah/api';
+    gapi.client.load('hukabuk', 'v1beta', hukABukApp.getGames, apiRoot);
+  } else {
+    document.getElementById('games').classList.add('hidden');
+  }
+
   var tokenEmail = hukABukApp.baseSigninCallback(authResult);
 };
 
